da1ba6a42c4b03f081727e186100ea61ba89a2e2
berlizz <44berlizz@gmail.com>
2017-06-15 14:17:36 +0900

    요구사항1 http://localhost:8080/index.html로 접속시 응답

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,14 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,10 +26,31 @@ public class RequestHandler extends Thread {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+        try (InputStream in = connection.getInputStream(); 
+        		OutputStream out = connection.getOutputStream();
+        		BufferedReader reader = new BufferedReader(new InputStreamReader(in)) ) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "Hello World".getBytes();
+            
+            String line = reader.readLine();
+			String[] strArr = new String[line.length()];
+			if(line != null) {
+				strArr = line.split(" ");
+			}
+			
+			while(!line.equals("")) {
+				if(!strArr[1].equals("/")) {
+					String filePath = "./webapp";
+			    	body = Files.readAllBytes(Paths.get(filePath + strArr[1]));
+				}
+				line = reader.readLine();
+				
+				if(line == null) {
+					break;
+				}
+			}
+            
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
